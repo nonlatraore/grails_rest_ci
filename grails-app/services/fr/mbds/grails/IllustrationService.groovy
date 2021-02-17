7f404c4b551869fd79ecca43d4cfe68849aeec8c
FabienneY <66945418+FabienneY@users.noreply.github.com>
2021-02-17 11:45:26 +0000

    3e commit

---
+++ grails-app/services/fr/mbds/grails/IllustrationService.groovy
@@ -0,0 +1,17 @@
+package fr.mbds.grails
+
+import grails.gorm.services.Service
+
+@Service(Illustration)
+interface IllustrationService {
+
+    Illustration get(Serializable id)
+
+    List<Illustration> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Illustration save(Illustration illustration)
+}
